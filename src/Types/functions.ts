6bccd4d3de8ff6d4f831b3f89507f1c0fe2e317d
Yash Pawar <yashpawarp@gmail.com>
2021-11-16 18:31:59 +0530

    woohoo callbacks

--- src/Types/functions.ts
+++ src/Types/functions.ts
@@ -10,3 +10,13 @@ let add: Function = (num1: number, num2: number) => num1 + num2;
 function caller(func: Function, ...params) {
     return func(...params);
 }
+
+// Callbacks
+
+// can specify return type and parameters of function callback too
+function specificCaller(fn: (n1:number) => string, num:number) {
+    console.log(fn(num));
+}
+
+specificCaller(f1, 12)
+specificCaller(add, 12) // yields an error
